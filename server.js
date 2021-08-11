558a0a32a23ca6e9dc46293e0186acfec4f2467c
hskuza <hubert.skuza2011@gmail.com>
2021-08-11 10:15:23 +0200

    database connection

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 
+const DB = process.env.DATABASE.replace(
+	"<PASSWORD>",
+	process.env.DATABASE_PASSWORD
+);
+mongoose
+	.connect(DB, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useFindAndModify: false,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("DB connection successfull!"));
+
 // route middleware
 readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
